5af392098886697a9d0136b3a0744caeda6b8392
Ford Henley <fordp86@gmail.com>
2022-02-11 17:22:35 -0600

    Complete Battleship Js
    Complete Battleship Js
    
    Page keeps loading but game works.

--- battleship.js
+++ battleship.js
@@ -1,28 +1,38 @@
+// Define Ship Location
 var location1 = 3;
 var location2 = 4;
 var location3 = 5;
 
+//Define Hits and Guesses
 var guess;
 var hits = 0;
 var guesses = 0;
 
+//Create isSunk Loop
+
 var isSunk = false;
 
 while (isSunk == false){
+    //Start Game
     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
     if(guess < 0 || guess > 6){
+        //Tell user to endter a number between one and 6
         alert("Please enter a valid cell number!");
     } else{
         guesses = guesses + 1;
-    }
+        //Define Hit Location
+        if(guess == location1 || guess == location2 || guess == location3){
 
-    if(guess == location1 || guess == location2 || guess == location3){
-        hits = hits + 1;
+            alert("HIT!");
+            hits = hits + 1;
 
-        if(hits == 3){
-            isSunk = true;
-
-            alert("You sank my battleship!");
+            if(hits == 3){
+                isSunk = true;
+    
+                alert("You sank my battleship!");
+            }
+        }else{
+            alert("MISS");
         }
     }
 }
