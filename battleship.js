a22b8109837bd5bac34b4134de2b88241a2c8593
Ford Henley <fordp86@gmail.com>
2022-02-11 16:15:09 -0600

    Added stats alert
    Added message to tell the user how they did.

--- battleship.js
+++ battleship.js
@@ -15,4 +15,17 @@ while (isSunk == false){
     } else{
         guesses = guesses + 1;
     }
+
+    if(guess == location1 || guess == location2 || guess == location3){
+        hits = hits + 1;
+
+        if(hits == 3){
+            isSunk = true;
+
+            alert("You sank my battleship!");
+        }
+    }
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " + " which means your shooting accuracy was " + (3/guesses);
+alert(stats);
