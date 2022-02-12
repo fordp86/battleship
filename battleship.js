5cf7fe97c2b79630f26963e0ed3a62a8e0695631
Ford Henley <fordp86@gmail.com>
2022-02-11 18:46:30 -0600

    Completed task

--- battleship.js
+++ battleship.js
@@ -1,7 +1,9 @@
 // Define Ship Location
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 
 //Define Hits and Guesses
 var guess;
